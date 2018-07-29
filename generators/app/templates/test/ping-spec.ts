5dd314d4262aa5d984d5dae82e76a5a117f89c88
unglaublicherdude <github@matthias-simonis.de>
2018-07-30 00:17:06 +0200

    Fixed the Unit-Test for Ping. Tried to import from a wrong path.

--- generators/app/templates/test/ping-spec.ts
+++ generators/app/templates/test/ping-spec.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-implicit-dependencies
-import ping from "../src/ping");
+import * as ping from "../src/handlers/ping";
 import * as chai from "chai";
 
 const expect = chai.expect;
